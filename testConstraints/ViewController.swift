918ca8204555f0a6de441a22520d9b37c3e87dc6
Sergey <poletvnebesa@yandex.ru>
2019-03-06 13:14:04 +0300

    Initial Commit

---
+++ testConstraints/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  testConstraints
+//
+//  Created by Sergey Mikhailov on 06/03/2019.
+//  Copyright © 2019 Sergey Mikhailov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
